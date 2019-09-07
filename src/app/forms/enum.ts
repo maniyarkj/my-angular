6e2b8ba4e858cdd0e7e820d4b15e47345b1215e6
kmaniyar <khilen.maniyar@gmail.com>
2019-09-07 07:26:37 +0530

    ngClass, ngSwitch

---
+++ src/app/forms/enum.ts
@@ -0,0 +1,3 @@
+export enum Direction {
+    East, West, North, South
+};
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,9 +1,12 @@
 import { Component, DoBootstrap } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Direction } from './enum';
+import { Direct } from 'protractor/built/driverProviders';
 
 @Component({
     selector: 'app-forms',
-    templateUrl: './forms.component.html'
+    templateUrl: './forms.component.html',
+    styleUrls: ['./forms.component.css']
 })
 
 export class FormsComponent {
@@ -14,39 +17,78 @@ export class FormsComponent {
     password;
     gender;
     mobile;
+    people = [];
+    direct = Direction;
+    myDir;
+    
+    
+    full_name = new FormControl('');
+    date_of_birth = new FormControl('', Validators.required);
+    emailId = new FormControl('', Validators.required);
+    mobile_number = new FormControl('', Validators.required);
+    password_control = new FormControl('', Validators.required);
+    c_password_control = new FormControl('', Validators.required);
+    member_gender = new FormControl('', Validators.required);
 
     constructor() {
-        const full_name = new FormControl('');
-        const date_of_birth = new FormControl('', Validators.required);
-        const emailId = new FormControl('', Validators.required);
-        const mobile_number = new FormControl('', Validators.required);
-        const password_control = new FormControl('', Validators.required);
-        const c_password_control = new FormControl('', Validators.required);
-        const member_gender = new FormControl('', Validators.required);
+        console.log(this.direct.East);
+        this.myDir = this.direct.East;
 
-        this.myForm = new FormGroup({
-            full_name: full_name,
-            date_of_birth: date_of_birth,
-            emailId: emailId,
-            mobile_number: mobile_number,
-            password_control: password_control,
-            c_password_control: c_password_control,
-            member_gender: member_gender,
-        })
+        // this.myForm = new FormGroup({
+        //     full_name: this.full_name,
+        //     date_of_birth: this.date_of_birth,
+        //     emailId: this.emailId,
+        //     mobile_number: this.mobile_number,
+        //     password_control: this.password_control,
+        //     c_password_control: this.c_password_control,
+        //     member_gender: this.member_gender,
+        // })
+
+
+        this.people.push(
+            {
+                id: 1,
+                name: 'khilen',
+                age: 34
+            },
+            {
+                id: 2,
+                name: 'Rakesh',
+                age: 24
+            },
+            {
+                id: 3,
+                name: 'Sanket',
+                age: 24
+            },
+            {
+                id: 4,
+                name: 'Janki',
+                age: 22
+            }
+        );
+
+        console.log(this.people);
 
     }
 
     submitData() : void {
 
-        console.log(this.myForm.valid);
+        // console.log(this.myForm.valid);
 
-        console.log(this.name);
-        console.log(this.dob);
-        console.log(this.email);
-        console.log(this.mobile);
-        console.log(this.password);
-        console.log(this.gender);
+        // console.log(this.name);
+        // console.log(this.dob);
+        // console.log(this.email);
+        // console.log(this.mobile);
+        // console.log(this.password);
+        // console.log(this.gender);
 
+        console.log(this.full_name.value);
 
     }
+
+    reloadFormData(): void {
+        this.mobile_number.setValue(9979328500);
+    }
+
 }
